cd674d61234c37af9894b038ee28e50c71941ed3
Arsyal Raka <55943145+ArsyalRaka02@users.noreply.github.com>
2022-10-31 02:16:11 +0700

    feat: list raport

--- src/screens/ListRaport.js
+++ src/screens/ListRaport.js
@@ -24,9 +24,55 @@ export default function ListRaport(props) {
                     <Text style={styles.txtHeader}>Raport</Text>
                 </HeaderBack>
                 <View style={{ padding: 20, flex: 1 }}>
-                   
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <View style={styles.childContainer}>
+                            <View style={{ flexDirection: 'column' }}>
+                                <Text>Kelas</Text>
+                                <Text>11 IPA 1</Text>
+                            </View>
+                            <View style={{ flexDirection: 'column' }}>
+                                <Text>Rata-rata</Text>
+                                <Text>80</Text>
+                            </View>
+                        </View>
+                        <View style={{ width: 30 }} />
+                        <View style={styles.childContainer}>
+                            <View style={{ flexDirection: 'column' }}>
+                                <Text>Kelas</Text>
+                                <Text>11 IPA 1</Text>
+                            </View>
+                            <View style={{ flexDirection: 'column' }}>
+                                <Text>Rata-rata</Text>
+                                <Text>80</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={{ flex: 1 }}>
+                        <Text style={[styles.txtGlobalBold, { marginVertical: 18 }]}>Nilai Pelajaran</Text>
+
+                        <ScrollView>
+                            <ListPelajaran />
+                        </ScrollView>
+                    </View>
                 </View>
             </SafeAreaView>
+        </>
+    )
+}
+
+function ListPelajaran() {
+    return (
+        <>
+            <View style={{ backgroundColor: color.white, paddingHorizontal: 20, paddingVertical: 12, borderRadius: 12, flexDirection: "row", alignItems: 'center' }}>
+                <Text style={[styles.txtGlobalBold, { flex: 1 }]}>Matematika</Text>
+                <View style={{ flexDirection: 'row', marginRight: 18 }}>
+                    <Text style={[styles.txtGlobal]}>Nilai : </Text>
+                    <Text style={[styles.txtGlobalBold]}>80</Text>
+                </View>
+                <Ionicons name="chevron-down-outline" size={24} color={color.black} />
+            </View>
         </>
     )
 }
@@ -43,4 +89,13 @@ const styles = {
     },
     txtGlobal: { fontSize: 14, fontFamily: fonts.inter },
     txtGlobalBold: { fontSize: 16, fontFamily: fonts.interBold },
+    childContainer: {
+        backgroundColor: color.white,
+        flexDirection: "row",
+        flex: 1,
+        borderRadius: 12,
+        justifyContent: 'space-between',
+        paddingHorizontal: 18,
+        paddingVertical: 12
+    },
 }
