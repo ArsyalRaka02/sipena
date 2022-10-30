3072833dc8c6a3ce13d0965a3b7252f4f88589cf
Arsyal Raka <55943145+ArsyalRaka02@users.noreply.github.com>
2022-10-31 03:19:10 +0700

    feat: list absen

--- src/screens/ListAbsen.js
+++ src/screens/ListAbsen.js
@@ -24,7 +24,23 @@ export default function ListAbsen(props) {
                     <Text style={styles.txtHeader}>Absen</Text>
                 </HeaderBack>
                 <View style={{ padding: 20, flex: 1 }}>
-                   
+                    <View style={{ backgroundColor: color.white, paddingVertical: 12, paddingHorizontal: 20, borderRadius: 12, flexDirection: 'row', alignItems: "center" }}>
+                        <View style={{ height: 79, width: 79, marginRight: 16 }}>
+                            <Image source={require("../assets/images/warning.png")} style={{ height: "100%", width: "100%" }} resizeMode="cover" />
+                        </View>
+                        <View style={{ flexDirection: 'column', flex: 1 }}>
+                            <Text style={[styles.txtGlobalBold, { color: color.black, fontSize: 14, marginBottom: 6 }]}>Lakukan absen sesuai dengan jadwal hari ini!</Text>
+                            <Text style={[styles.txtGlobal, { fontSize: 13 }]}>Absensi menggunakan QRCODE</Text>
+                        </View>
+                    </View>
+
+                    <View style={{ height: 20 }} />
+
+                    <View style={{ flexDirection: 'row', backgroundColor: color.white, borderRadius: 12, paddingHorizontal: 20, paddingVertical: 16 }}>
+                        <Text style={[styles.txtGlobalBold, { color: color.black }]}>Total Kehadiran</Text>
+                        <View style={{ flex: 1 }} />
+                        <Text style={[styles.txtGlobalBold, { color: color.primary }]}>Lihat Sekarang</Text>
+                    </View>
                 </View>
             </SafeAreaView>
         </>
