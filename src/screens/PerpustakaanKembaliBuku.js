0922c26faac7734911fbcc0249dbf2f4636e3004
Arsyal Raka <55943145+ArsyalRaka02@users.noreply.github.com>
2022-11-16 20:29:03 +0700

    feat: perpustakaan kembalikan buku

--- src/screens/PerpustakaanKembaliBuku.js
+++ src/screens/PerpustakaanKembaliBuku.js
@@ -7,6 +7,7 @@ import { useNavigation } from '@react-navigation/native'
 import TextInputIcon from '../components/TextInputIcon'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { fonts } from '../utils/fonts'
+import Button from '../components/Button'
 
 const SCREEN_HEIGHT = Dimensions.get("window").height
 const SCREEN_WIDTH = Dimensions.get("window").width
@@ -24,9 +25,80 @@ export default function PerpustakaanKembaliBuku(props) {
                     <Text style={styles.txtHeader}>Kembalikan Buku</Text>
                 </HeaderBack>
                 <View style={{ padding: 20, flex: 1 }}>
+                    <View style={{ backgroundColor: color.white, paddingVertical: 12, paddingHorizontal: 20, borderRadius: 12, flexDirection: 'row', alignItems: "center" }}>
+                        <View style={{ height: 79, width: 79, marginRight: 16 }}>
+                            <Image source={require("../assets/images/warning.png")} style={{ height: "100%", width: "100%" }} resizeMode="cover" />
+                        </View>
+                        <View style={{ flexDirection: 'column', flex: 1 }}>
+                            <Text style={[styles.txtGlobalBold, { color: color.black, fontSize: 14, marginBottom: 6 }]}>Durasi Peminjaman Buku 1 Minggu</Text>
+                            <Text style={[styles.txtGlobal, { fontSize: 13 }]}>Jika lebih dari itu denda seribu perhari</Text>
+                        </View>
+                    </View>
 
+                    {/* //list buku */}
+                    <View style={{ flex: 1 }}>
+                        <ScrollView>
+                            <ListKatalog />
+                            <Text style={[styles.txtGlobalBold, { fontSize: 18, marginVertical: 20, color: color.black }]}>Total Denda</Text>
+                            <TextInputIcon
+                                editable={false}
+                                value={"Denda"}
+                                wrapperStyle={{ backgroundColor: color.themeGray, borderColor: color.themeGray }}
+                            />
+                        </ScrollView>
+                    </View>
+                </View>
+                <View style={{ backgroundColor: color.white, paddingTop: 40, paddingBottom: 20, paddingHorizontal: 20 }}>
+                    <Button
+                        onPress={() => {
+
+                        }}
+                    >
+                        Kembalikan Buku
+                    </Button>
                 </View>
             </SafeAreaView>
+        </>
+    )
+}
+
+function ListKatalog() {
+    let data = [
+        {
+            judul: "Unforgettable",
+            creator: "By ArsyalDev",
+            day: "8 day"
+        },
+        {
+            judul: "Unforgettable",
+            creator: "By ArsyalDev",
+            day: "8 day"
+        },
+        {
+            judul: "Unforgettable",
+            creator: "By ArsyalDev",
+            day: "8 day"
+        },
+
+    ]
+    return (
+        <>
+            <Text style={[styles.txtGlobalBold, { fontSize: 18, color: color.black, marginVertical: 20 }]}>Daftar Buku</Text>
+            {
+                data.map((item, iList) => {
+                    return (
+                        <>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: color.white, borderRadius: 12, paddingHorizontal: 20, paddingVertical: 10, marginBottom: 10 }}>
+                                <View style={{ flexDirection: 'column', flex: 1 }}>
+                                    <Text style={[styles.txtGlobalBold, { fontSize: 14, color: color.black }]}>{item.judul}</Text>
+                                    <Text style={[styles.txtGlobal, { fontSize: 13, color: color.black }]}>{item.creator}</Text>
+                                </View>
+                                <Text>{item.day}</Text>
+                            </View>
+                        </>
+                    )
+                })
+            }
         </>
     )
 }
