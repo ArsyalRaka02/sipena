008b61e02309a33d6a1592b2fd786102944ecd2e
Arsyal Raka <55943145+ArsyalRaka02@users.noreply.github.com>
2022-11-17 01:25:33 +0700

    feat: perpustakaan sumbang buku

--- src/screens/PerpustakaanSumbangBuku.js
+++ src/screens/PerpustakaanSumbangBuku.js
@@ -7,12 +7,26 @@ import { useNavigation } from '@react-navigation/native'
 import TextInputIcon from '../components/TextInputIcon'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { fonts } from '../utils/fonts'
+import Combobox from '../components/Combobox'
+import Button from '../components/Button'
 
 const SCREEN_HEIGHT = Dimensions.get("window").height
 const SCREEN_WIDTH = Dimensions.get("window").width
 
+const listBukuHilang = [
+    {
+        id: "Unforgettable",
+        label: "Unforgettable"
+    },
+    {
+        id: "Unforgettable",
+        label: "Unforgettable"
+    },
+]
+
 export default function PerpustakaanSumbangBuku(props) {
     const navigation = useNavigation()
+    const [selectedBuku, setSelectedBuku] = useState(null)
     return (
         <>
             <SafeAreaView style={styles.container}>
@@ -24,7 +38,56 @@ export default function PerpustakaanSumbangBuku(props) {
                     <Text style={styles.txtHeader}>Sumbang Buku</Text>
                 </HeaderBack>
                 <View style={{ padding: 20, flex: 1 }}>
+                    <ScrollView>
+                        <Text style={[styles.txtGlobalBold, { fontSize: 14, color: color.black, marginVertical: 10 }]}>Nama Buku</Text>
+                        <TextInputIcon
+                            value={"nama buku"}
+                        />
+                        <Text style={[styles.txtGlobalBold, { fontSize: 14, color: color.black, marginVertical: 10 }]}>Nama Penulis</Text>
+                        <TextInputIcon
+                            value={"nama Penulis"}
+                        />
 
+                        <Text style={[styles.txtGlobalBold, { fontSize: 14, color: color.black, marginVertical: 10 }]}>Kategori Buku</Text>
+                        <Combobox
+                            value={selectedBuku}
+                            placeholder="Silahkan Pilih Buku"
+                            theme={{
+                                boxStyle: {
+                                    backgroundColor: color.white,
+                                    borderColor: color.Neutral20,
+                                },
+                                leftIconStyle: {
+                                    color: color.Neutral10,
+                                    marginRight: 14
+                                },
+                                rightIconStyle: {
+                                    color: color.Neutral10,
+                                },
+                            }}
+                            jenisIconsRight="Ionicons"
+                            iconNameRight="caret-down-outline"
+                            showLeftIcons={false}
+                            data={listBukuHilang}
+                            onChange={(val) => {
+                                setSelectedBuku(val);
+                            }}
+                        />
+
+                        <Text style={[styles.txtGlobalBold, { fontSize: 14, color: color.black, marginVertical: 10 }]}>Jumlah Halaman</Text>
+                        <TextInputIcon
+                            value={"jumlah halaman"}
+                        />
+                        <Text style={[styles.txtGlobalBold, { fontSize: 14, color: color.black, marginVertical: 10 }]}>Bahasa</Text>
+                        <TextInputIcon
+                            value={"bahasa"}
+                        />
+                    </ScrollView>
+                </View>
+                <View style={{ backgroundColor: color.white, paddingTop: 40, paddingBottom: 20, paddingHorizontal: 20 }}>
+                    <Button>
+                        Sumbang Sekarang
+                    </Button>
                 </View>
             </SafeAreaView>
         </>
