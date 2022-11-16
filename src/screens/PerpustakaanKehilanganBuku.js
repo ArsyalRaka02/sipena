899365b93439119b5b26584cfd30af8e9f7476db
Arsyal Raka <55943145+ArsyalRaka02@users.noreply.github.com>
2022-11-17 01:20:17 +0700

    feat: perpustakaan kehilangan buku

--- src/screens/PerpustakaanKehilanganBuku.js
+++ src/screens/PerpustakaanKehilanganBuku.js
@@ -7,12 +7,29 @@ import { useNavigation } from '@react-navigation/native'
 import TextInputIcon from '../components/TextInputIcon'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { fonts } from '../utils/fonts'
+import Dropdown from '@febfeb/react-native-dropdown'
+import Combobox from '../components/Combobox'
+import Button from '../components/Button'
 
 const SCREEN_HEIGHT = Dimensions.get("window").height
 const SCREEN_WIDTH = Dimensions.get("window").width
 
+const listBukuHilang = [
+    {
+        id: "Unforgettable",
+        label: "Unforgettable"
+    },
+    {
+        id: "Unforgettable",
+        label: "Unforgettable"
+    },
+]
+
 export default function PerpustakaanKehilanganBuku(props) {
     const navigation = useNavigation()
+
+    const [selectedBuku, setSelectedBuku] = useState(null)
+
     return (
         <>
             <SafeAreaView style={styles.container}>
@@ -24,7 +41,52 @@ export default function PerpustakaanKehilanganBuku(props) {
                     <Text style={styles.txtHeader}>Kehilangan Buku</Text>
                 </HeaderBack>
                 <View style={{ padding: 20, flex: 1 }}>
+                    <ScrollView>
+                        <Text style={[styles.txtGlobalBold, { fontSize: 16, color: color.black, marginVertical: 12 }]}>Buku Hilang</Text>
+                        <Combobox
+                            value={selectedBuku}
+                            placeholder="Silahkan Pilih Buku"
+                            theme={{
+                                boxStyle: {
+                                    backgroundColor: color.white,
+                                    borderColor: color.Neutral20,
+                                },
+                                leftIconStyle: {
+                                    color: color.Neutral10,
+                                    marginRight: 14
+                                },
+                                rightIconStyle: {
+                                    color: color.Neutral10,
+                                },
+                            }}
+                            jenisIconsRight="Ionicons"
+                            iconNameRight="caret-down-outline"
+                            showLeftIcons={false}
+                            data={listBukuHilang}
+                            onChange={(val) => {
+                                setSelectedBuku(val);
+                            }}
+                        />
 
+                        <Text style={[styles.txtGlobalBold, { fontSize: 16, color: color.black, marginVertical: 12 }]}>Saldo Anda</Text>
+                        <TextInputIcon
+                            editable={false}
+                            value={"200.000"}
+                            wrapperStyle={{ backgroundColor: color.themeGray, borderColor: color.themeGray }}
+                        />
+
+                        <Text style={[styles.txtGlobalBold, { fontSize: 16, color: color.black, marginVertical: 12 }]}>Nominal Pembayaran</Text>
+                        <TextInputIcon
+                            editable={false}
+                            value={"50.000"}
+                            wrapperStyle={{ backgroundColor: color.themeGray, borderColor: color.themeGray }}
+                        />
+                    </ScrollView>
+                </View>
+                <View style={{ backgroundColor: color.white, paddingTop: 40, paddingBottom: 20, paddingHorizontal: 20 }}>
+                    <Button>
+                        Bayar Sekarang
+                    </Button>
                 </View>
             </SafeAreaView>
         </>
